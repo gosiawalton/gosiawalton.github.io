dc3422cd2358222420af999bf077c7462e98d7cc
Clément Mouchet <clement.mouchet@gmail.com>
2015-10-12 23:32:10 +0100

    Initial commit

---
+++ js/background_video.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function($) {
+  var video = document.getElementById("background_video");
+  var pauseButton = document.querySelector(".pause button");
+
+  function fadeVideo() {
+    video.classList.add("video_overlay");
+  }
+
+  video.addEventListener('ended', function() {
+    // only functional if "loop" is removed
+    video.pause();
+    // to capture IE10
+    fadeVideo();
+  });
+
+  pauseButton.addEventListener("click", function() {
+    video.classList.toggle("video_overlay");
+    if (video.paused) {
+      video.play();
+      pauseButton.innerHTML = $(pauseButton).html().replace('On', 'Off');
+    } else {
+      video.pause();
+      pauseButton.innerHTML = $(pauseButton).html().replace('Off', 'On');
+    }
+  });
+});
