7a2854df017112de1f0a2eb2b728cbd535b84d6a
Clément Mouchet <clement.mouchet@gmail.com>
2015-10-13 08:27:55 +0100

    try to fix autoplay on iOS

--- js/background_video.js
+++ js/background_video.js
@@ -2,8 +2,6 @@ jQuery(document).ready(function($) {
   var video = document.getElementById("background_video");
   var pauseButton = document.querySelector(".pause button");
 
-  video.play();
-
   function fadeVideo() {
     video.classList.add("video_overlay");
   }
