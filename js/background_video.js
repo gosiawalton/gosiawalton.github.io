7b6510191119a0bcb3e651af7d4f5752ec959158
Clément Mouchet <clement.mouchet@gmail.com>
2015-10-13 19:06:43 +0100

    refactored js to support click to play & pause, to workaround the inability to autoplay on iOS

--- js/background_video.js
+++ js/background_video.js
@@ -2,25 +2,30 @@ jQuery(document).ready(function($) {
   var video = document.getElementById("background_video");
   var pauseButton = document.querySelector(".pause button");
 
-  function fadeVideo() {
-    video.classList.add("video_overlay");
+  function toggleVideoPauseState() {
+    if (video.paused) {
+      video.play();
+      video.classList.remove("video_overlay");
+      pauseButton.innerHTML = $(pauseButton).html().replace('On', 'Off');
+    } else {
+      video.pause();
+      video.classList.add("video_overlay");
+      pauseButton.innerHTML = $(pauseButton).html().replace('Off', 'On');
+    }
   }
 
   video.addEventListener('ended', function() {
     // only functional if "loop" is removed
     video.pause();
     // to capture IE10
-    fadeVideo();
+    video.classList.add("video_overlay");
+  });
+
+  video.addEventListener('click', function() {
+    toggleVideoPauseState();
   });
 
   pauseButton.addEventListener("click", function() {
-    video.classList.toggle("video_overlay");
-    if (video.paused) {
-      video.play();
-      pauseButton.innerHTML = $(pauseButton).html().replace('On', 'Off');
-    } else {
-      video.pause();
-      pauseButton.innerHTML = $(pauseButton).html().replace('Off', 'On');
-    }
+    toggleVideoPauseState()
   });
 });
